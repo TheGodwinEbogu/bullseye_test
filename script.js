0cdd50bd22b46a91608389079fcd09c89e335d7f
Ebogu Okechukwu <Ebogu.O@bullseye.local>
2023-06-10 15:21:19 +0100

    final commit

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 document.querySelector(".toggle-on").addEventListener("click", () => {
   document.querySelector(".toggle-on").style.display = "none";
   document.querySelector(".toggle-off").style.display = "block";
+  document.querySelector("#cart1").textContent = "$199.9";
+  document.querySelector("#cart2").textContent = "$249.9";
+  document.querySelector("#cart3").textContent = "$399.99";
 });
 
 document.querySelector(".toggle-off").addEventListener("click", () => {
   document.querySelector(".toggle-on").style.display = "block";
   document.querySelector(".toggle-off").style.display = "none";
+  document.querySelector("#cart1").textContent = "$19.9";
+  document.querySelector("#cart2").textContent = "$24.9";
+  document.querySelector("#cart3").textContent = "$39.99";
 });
